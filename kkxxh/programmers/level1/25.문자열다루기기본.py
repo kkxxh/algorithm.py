2e8adcd200ba06a49cac2e98d3b804a3e00fef7f
kangseonghee <seonghhee@gmail.com>
2022-10-22 16:29:45 +0900

    _

---
+++ kkxxh/programmers/level1/25.문자열다루기기본.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = True
+    number = ["1","2","3","4","5","6","7","8","9","0"]
+    for i in s:
+        if i not in number:
+            answer = False
+    return answer
