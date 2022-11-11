a3619366807bc52fabd0e14b1aeeaa22fcb3bfff
kimjunkyung <junkyung.kk@gmail.com>
2022-11-11 23:43:28 +0900

    solved_20221111

---
+++ junnkk/Algorithm/Greedy/practice/곱하기혹은더하기.py
@@ -0,0 +1,12 @@
+n = input()
+
+result = 0
+
+for i in range(len(n)):
+    if result == 0 or n[i] == '0' or n[i] == '1':
+        result += int(n[i])
+    else:
+        result *= int(n[i])
+
+
+print(result)
