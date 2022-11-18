4b87fafd2d9eac3f747308a74900c0eb757a1c77
kimjunkyung <junkyung.kk@gmail.com>
2022-11-18 23:45:00 +0900

    -

---
+++ junnkk/Algorithm/Implementation/practice/게임개발.py
@@ -0,0 +1,17 @@
+# n, m = map(int, input().split())
+# a, b, d = map(int, input().split())
+
+# game_map = []
+
+# # 북, 동, 남, 서
+# direction = [0, 1, 2, 3]
+
+# for _ in range(m):
+#     game_map.append(list(map(int, input().split())))
+
+# while True:
+#     for i in range(len(direction)):
+#         if d == direction[i] and direction[i-1]
+
+
+# print(game_map)
